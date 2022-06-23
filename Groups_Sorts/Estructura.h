7a702642ae1289c752d441e8f634160b6e281211
marlyn-almeida <105186016+marlyn-almeida@users.noreply.github.com>
2022-06-23 18:53:06 -0500

    Add files via upload

---
+++ Groups_Sorts/Estructura.h
@@ -0,0 +1,40 @@
+/*      UNIVERSIDAD DE LAS FUERZAS ARMADAS
+*       Ordenamiento Mezcla Natural
+*       Autor: Almeida Marlyn, Chicango George
+*       Fecha de creación: 22/06/2022
+*       Fecha de modificación: 23/06/2022
+*       Grupo: 1
+*       Github: https://github.com/cfidrobo/Estructura_de_Datos_TallerGrupal
+*/
+
+#pragma once
+#include <iostream>
+#include "Arreglo.h"
+
+template <typename T>
+
+class Burbuja{
+	
+	
+ 
+};
+class QuickSort{	
+
+	
+ 
+};
+class ShellSort{
+	
+	
+ 
+};
+class Distribucion{
+	
+	
+ 
+};
+class Radix{
+	
+	
+ 
+};
