7cc4ce7cc43dbe36a9ed26d800cac2ae949daa5f
vacamacho1 <105189898+vacamacho1@users.noreply.github.com>
2022-06-26 14:48:55 -0500

    Ordenamiento ShellSort por Grupo 16

--- Groups_Sorts/Ordenamiento.cpp
+++ Groups_Sorts/Ordenamiento.cpp
@@ -14,7 +14,20 @@ class Quicksort: public InterfazQuicksort<T>
 };
 class Shellsort: public InterfazShellsort<T>
 {
+ for (int gap = T.get_tamano / 2; gap > 0; gap /= 2)
+	{
+		for (int i = gap ; i < T.get_tamano ; i += 1)
+		{
+			int T.get_tamano = *(T + i);
 
+			int j;
+			for (j = i; j >= gap && *(T + (j - gap)) > temp; j -= gap) {
+				*(T + j) = *(T + (j - gap));
+			}
+
+			*(T + j) = temp;
+		}
+	}
 };
 class Distribucion: public InterfazDistribucion<T>
 {
