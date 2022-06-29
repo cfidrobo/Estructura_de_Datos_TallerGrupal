fcf0c86e371d237aca84bfa75b49dedba3a04d0e
vacamacho1 <105189898+vacamacho1@users.noreply.github.com>
2022-06-28 23:34:27 -0500

    Encerar

--- Taller Ordenamiento/Groups_Sorts/Arreglo.h
+++ Taller Ordenamiento/Groups_Sorts/Arreglo.h
@@ -93,9 +93,7 @@ T Arreglo<T>::obtener(int indice)
 
 template<typename T>
 void Arreglo<T>::encerar(){
-    for(int i =0; i <_tamano; i++){
-		*(_datos+i)=NULL;
-	}
+    delete[] _datos;
 }
 
 template <typename T>
